ad9e45bd0eb5a6f3b10fb9f8210c3e499333a767
theClubhou.se <marsackg7@gmail.com>
2019-07-17 16:03:17 -0400

    4th

--- app.js
+++ app.js
@@ -1,42 +1,53 @@
-// // var http = require('http');
 
-// // var PORT = 8080;
-// // var server = http.createServer((request, response) => {
+/* Need this top half to create server and 
+then talk to DB via mysql
+DB will have the addresses and then further tags to sort based
+on School Ranking, Price and find Homes by searching City, State & zip code
+This top half breaks the bottom half so be sure to comment it out
+if you just want to run the on click show array*/
 
-// // });
+var http = require('http');
 
-// // server.listen(PORT, () => console.log('server is listening on port {8080}'));
+var PORT = 8080;
+var server = http.createServer((request, response) => {
 
-// // var express =require('express');
-// var mysql = require('mysql');
-// // var app = express();
+});
 
-// var connect =mysql.createConnection({
-//     host:"locacalhost",
-//     user: 'root',
-//     password: 'root',
-//     database: 'homeDB',
-// });
+server.listen(PORT, () => console.log('server is listening on port {8080}'));
 
-// connect.connect(function(error){
-//     if (error){
-//         console.log('Error');
-//     } else {
-//         console.log('Connected');
-//     }
-// });
+// var express =require('express');
+var mysql = require('mysql');
+// var app = express();
 
-// mysql.get('/', function(req, res){
-//     connection.query("SELECT * FROM homeDB", function(error, rows, fields){
-//         if (error){
-//             console.log('Error in the query');
-//         } else {
-//             console.log('Successful Query');
-//         }
-//     });
-// })
+var connect =mysql.createConnection({
+    host:"locacalhost",
+    user: 'root',
+    password: 'root',
+    database: 'homeDB',
+});
 
-// mysql.listen(8080);
+connect.connect(function(error){
+    if (error){
+        console.log('Error');
+    } else {
+        console.log('Connected');
+    }
+});
+
+mysql.get('/', function(req, res){
+    connection.query("SELECT * FROM homeDB", function(error, rows, fields){
+        if (error){
+            console.log('Error in the query');
+        } else {
+            console.log('Successful Query');
+        }
+    });
+})
+
+mysql.listen(8080);*/
+
+
+/*From here down will have the on click load the array*/
 
 var btn = document.getElementById("btn");
 var list = document.getElementById("addressList");
