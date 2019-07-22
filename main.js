f4df288fff13462f2762ca4eb57c7cd88e75b1c2
theClubhou.se <marsackg7@gmail.com>
2019-07-22 15:40:24 -0400

    making progress

--- main.js
+++ main.js
@@ -5,20 +5,19 @@ var list = document.getElementById("addressList");
 
 var addresses = ['614 Mockingbird Lane', '88 James Drive', '112 13th Street'];
 
-var btn = document.getElementById("btn");
-btn.addEventListener("click", function load(file){
-    document.getElementById("content").innerHTML='<object type="text/html" data="/login.html"></object>';
-
-});
-
 btn.addEventListener("click", function(){
     for (i=0; i<addresses.length; i++){
         var node = document.createElement("LI");
         var address = document.createTextNode(addresses[i]);
         node.appendChild(address);
-        list.appendChild(node);
+        list.appendChild(node);  
     }
+    console.log(addresses);
 });
+
+
+
+
 
 
 
